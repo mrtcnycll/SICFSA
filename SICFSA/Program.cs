e0ceca730d6e48cd0977b7198e21efc812ece7aa
mrtcnycll <139801843+mrtcnycll@users.noreply.github.com>
2023-07-18 20:18:25 +0300

    Update Program.cs

--- SICFSA/Program.cs
+++ SICFSA/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic.FileIO;
 using System;
+using System.ComponentModel;
 using System.IO;
 
 namespace SICFSA
@@ -11,16 +13,41 @@ namespace SICFSA
 
             newUser register = new newUser();
 
-            Console.WriteLine("Enter your name: ");
-            register.firstName = Console.ReadLine();
-            
-            Console.WriteLine("Enter your surname:");
-            register.lastName = Console.ReadLine();
+            Console.WriteLine("What would you like to do? ");
+            Console.WriteLine("Add student, Delete student or display students");
+            Console.WriteLine("----------------------------------------------");
+            string x = Console.ReadLine().ToLower();
 
-            Console.WriteLine("Enter your age: ");
-            register.age = Convert.ToInt32(Console.ReadLine());
+            if (x == "add student" || x == "add")
+            {
+                Console.WriteLine("Enter your name: ");
+                register.firstName = Console.ReadLine();
 
-            SaveNewUser(register);
+                Console.WriteLine("Enter your surname:");
+                register.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter your age: ");
+                register.age = Convert.ToInt32(Console.ReadLine());
+
+                SaveNewUser(register);
+            }
+            else if (x == "delete student" || x == "delete")
+            {
+                Console.WriteLine("----------------------------");
+                DisplaySavedUsers();
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Enter the index of the student you want to delete:");
+                int indexToDelete = Convert.ToInt32(Console.ReadLine());
+                DeleteUser(indexToDelete);
+            }
+            else if (x == "display student" || x=="display")
+            {
+                DisplaySavedUsers();
+            }
+            else
+            {
+                Console.WriteLine("You cant do that");
+            }
         }
             
         static void SaveNewUser(newUser User)
@@ -28,6 +55,26 @@ namespace SICFSA
 
             File.AppendAllText("save.txt", $"{User.firstName}-{User.lastName}-{User.age} {Environment.NewLine}");
             string readText = File.ReadAllText("Save.txt");
+        }
+        static void DisplaySavedUsers()
+        {
+            string readText = File.ReadAllText("save.txt");
+            Console.WriteLine(Environment.NewLine +"Saved Students:" + Environment.NewLine);
+            Console.WriteLine(readText);
+        }
+        static void DeleteUser(int index)
+        {
+            string[] lines = File.ReadAllLines("save.txt");
+            if (index >= 1 && index <= lines.Length)
+            {
+                lines[index - 1] = null;
+                File.WriteAllLines("save.txt", lines);
+                Console.WriteLine("Student deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid index no student deleted!");
+            }
         }
     }
         class newUser
