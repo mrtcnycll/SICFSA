950581f312b08ffc998aa587fc2f32e779e71e9e
mrtcnycll <mrtcnycl@gmail.com>
2023-07-18 15:18:47 +0300

    Proje dosyası ekle.

---
+++ SICFSA/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace SICFSA
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                newUser register = new newUser();
+
+                Console.WriteLine("Enter your name: ");
+
+                register.firstName = Console.ReadLine();
+
+
+                Console.WriteLine("Enter your surname:");
+                register.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter your age:");
+                register.age = Convert.ToInt32(Console.ReadLine());
+
+
+                File.AppendAllText("save.txt", $"{register.firstName}-{register.lastName}-{register.age} {Environment.NewLine}");
+
+                string readText = File.ReadAllText("Save.txt");
+            }
+
+        }
+        class newUser
+        { 
+            public string firstName = "";
+            public string lastName = "";
+            public int age = 0;
+        }
+}
