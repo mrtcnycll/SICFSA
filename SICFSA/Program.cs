f077524af12b5bec9db485414c15b87cd729118f
mrtcnycll <mrtcnycl@gmail.com>
2023-07-18 17:55:06 +0300

    de

--- SICFSA/Program.cs
+++ SICFSA/Program.cs
@@ -3,30 +3,35 @@ using System.IO;
 
 namespace SICFSA
 
+{
+    class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                newUser register = new newUser();
-
-                Console.WriteLine("Enter your name: ");
-
-                register.firstName = Console.ReadLine();
-
-
-                Console.WriteLine("Enter your surname:");
-                register.lastName = Console.ReadLine();
-
-                Console.WriteLine("Enter your age:");
-                register.age = Convert.ToInt32(Console.ReadLine());
-
-
-                File.AppendAllText("save.txt", $"{register.firstName}-{register.lastName}-{register.age} {Environment.NewLine}");
-
-                string readText = File.ReadAllText("Save.txt");
-            }
+        SaveNewUser();
         }
+            
+        static void SaveNewUser()
+        {
+            newUser register = new newUser();
+
+            Console.WriteLine("Enter your name: ");
+
+            register.firstName = Console.ReadLine();
+
+
+            Console.WriteLine("Enter your surname:");
+            register.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            register.age = Convert.ToInt32(Console.ReadLine());
+
+
+            File.AppendAllText("save.txt", $"{register.firstName}-{register.lastName}-{register.age} {Environment.NewLine}");
+
+            string readText = File.ReadAllText("Save.txt");
+        }
+    }
         class newUser
         { 
             public string firstName = "";
